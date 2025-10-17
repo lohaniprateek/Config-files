4d1d47d02e4e0df589ed532080e0715726a7c193
Prateek Kumar <kprateek9315@gmail.com>
2025-10-17 21:43:48 +0530

    update backup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s $HOME/Config-files/nvim $HOME/.config/nvim
+ln -s $HOME/Config-files/waybar $HOME/.config/waybar
+ln -s $HOME/Config-files/hypr $HOME/.config/hypr
+ln -s $HOME/Config-files/qutebrowser $HOME/.config/qutebrowser
+ln -s $HOME/Config-files/.zshrc $HOME/.zshrc
